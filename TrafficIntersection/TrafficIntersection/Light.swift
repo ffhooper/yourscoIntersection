cb2ae896354b7165febf7c0de067652da658ca8a
Riley Hooper <ffhooper@gmail.com>
2017-11-20 22:36:21 -0700

    Built UI.

---
+++ TrafficIntersection/TrafficIntersection/Light.swift
@@ -0,0 +1,14 @@
+//
+//  Light.swift
+//  TrafficIntersection
+//
+//  Created by Riley Hooper on 11/20/17.
+//  Copyright © 2017 Riley Hooper. All rights reserved.
+//
+
+import Foundation
+
+struct light {
+    var postion: String?
+     
+}
